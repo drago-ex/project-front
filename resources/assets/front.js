0236942f820dcb0d8a97d1704c4795a701ad8db7
Zdeněk Papučík <zdenek.papucik@gmail.com>
2026-05-12 12:33:54 +0200

    Update front.js

--- resources/assets/front.js
+++ resources/assets/front.js
@@ -1,5 +1,5 @@
 // these JS + SCSS will be automatically available after installing the package
-import {registerNajaExtensions} from "./core/base.js";
+import { registerNajaExtensions } from "./core/base.js";
 
 registerExtensions(
 	// naja extensions
