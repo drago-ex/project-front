8641b552b9cc0b8fdd0ab1797013b2a3b3d8b94b
Zdeněk Papučík <zdenek.papucik@gmail.com>
2026-06-10 11:49:12 +0200

    Update HomePresenter.php

--- resources/app/UI/Front/Home/HomePresenter.php
+++ resources/app/UI/Front/Home/HomePresenter.php
@@ -7,10 +7,7 @@ namespace App\UI\Front\Home;
 use App\UI\BasePresenter;
 
 
-/**
- * Home presenter handles the logic for the home page.
- * @property-read HomeTemplate $template
- */
+/** @property-read HomeTemplate $template */
 final class HomePresenter extends BasePresenter
 {
 }
