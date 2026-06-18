bd995596c85dd68b176b53b9cefc91f88b3e3dc6
accgit <zdenek.papucik@gmail.com>
2026-06-18 08:23:07 +0200

    Update

--- resources/assets/front.js
+++ resources/assets/front.js
@@ -1,5 +1,7 @@
 // these JS + SCSS will be automatically available after installing the package
 import { registerNajaExtensions } from "./core/base.js";
+
+// page styles
 import "./front.scss";
 
 registerNajaExtensions(
